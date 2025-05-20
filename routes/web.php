30d602679d6cae560eec22f7bf20af883e973b88
Balaji-11-udayasuriyan <balajiudayasuriyan1102@gmail.com>
2025-05-20 21:44:43 +0530

    add routes for posts index, show, and comment submission

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\PostController;
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
+Route::get('/post/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::post('/post/{post}/comment', [PostController::class, 'addComment'])->name('posts.comment');
+
